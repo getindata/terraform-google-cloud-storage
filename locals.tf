89ee691a519804fac9c76eb1fee6b0a8ea45a3db
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-01-09 10:28:12 +0100

    feat: Updates

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   name_from_descriptor = trim(replace(
-  lookup(module.this.descriptors, var.descriptor_name, module.this.id), "/${module.this.delimiter}${module.this.delimiter}+/", module.this.delimiter
+    lookup(module.this.descriptors, var.descriptor_name, module.this.id), "/${module.this.delimiter}${module.this.delimiter}+/", module.this.delimiter
   ), module.this.delimiter)
   enabled                    = module.this.enabled
   encrypt_gcs_bucket_tfstate = local.enabled && var.encrypt_gcs_bucket_tfstate
